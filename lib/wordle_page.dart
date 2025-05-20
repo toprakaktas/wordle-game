bb96cc5ff723d88978a4fdfbc855b7455df7f2ae
Toprak <topraktas19@hotmail.com>
2025-05-20 23:49:29 +0300

    buttons added & board fixed

--- lib/wordle_page.dart
+++ lib/wordle_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wordle_clone/keyboard_view.dart';
 import 'package:wordle_clone/wordle_provider.dart';
 import 'package:wordle_clone/wordle_view.dart';
 
@@ -25,19 +26,62 @@ class _WordlePageState extends State<WordlePage> {
         child: Column(
           children: [
             Expanded(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Consumer<WordleProvider>(
+                  builder:
+                      (context, provider, child) => GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 5,
+                          mainAxisSpacing: 5,
+                          crossAxisSpacing: 5,
+                        ),
+                        itemCount: provider.board.length,
+                        itemBuilder: (context, index) {
+                          final wordle = provider.board[index];
+                          return WordleView(wordle: wordle);
+                        },
+                      ),
+                ),
+              ),
+            ),
+            Consumer<WordleProvider>(
+              builder:
+                  (context, provider, child) => KeyboardView(
+                    excludedLetters: provider.excludedLetters,
+                    onPress: (value) {
+                      provider.inputLetter(value);
+                    },
+                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Consumer<WordleProvider>(
                 builder:
-                    (context, provider, child) => GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 5,
-                        mainAxisSpacing: 4,
-                        crossAxisSpacing: 4,
-                      ),
-                      itemCount: provider.board.length,
-                      itemBuilder: (context, index) {
-                        final wordle = provider.board[index];
-                        return WordleView(wordle: wordle);
-                      },
+                    (context, provider, child) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            provider.deleteLetter();
+                          },
+                          child: const Text('DELETE'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (!provider.isValidWord) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text(
+                                    'This word is not in the dictionary.',
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                          child: const Text('CHECK'),
+                        ),
+                      ],
                     ),
               ),
             ),
