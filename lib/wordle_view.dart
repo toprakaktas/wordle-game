65c8b46c67e61956659d9df71525a51842dc6ba9
Toprak <topraktas19@hotmail.com>
2025-05-22 00:13:20 +0300

    colors improved

--- lib/wordle_view.dart
+++ lib/wordle_view.dart
@@ -13,10 +13,12 @@ class WordleView extends StatelessWidget {
         shape: BoxShape.circle,
         color:
             wordle.existsInTarget
-                ? Colors.grey
+                ? const Color(0xFFC9B458) // Yellow (wrong spot)
                 : wordle.doesNotExistsInTarget
-                ? Colors.transparent
-                : null,
+                ? const Color(0xFF787C7E) // Gray (not in word)
+                : wordle.isCorrectSpot
+                ? const Color(0xFF6AAA64) // Green (correct spot)
+                : Colors.transparent, // Defa
         border: Border.all(
           color: Color.fromARGB(255, 64, 120, 103),
           width: 1.75,
@@ -26,12 +28,7 @@ class WordleView extends StatelessWidget {
         wordle.letter,
         style: TextStyle(
           fontSize: 16,
-          color:
-              wordle.existsInTarget
-                  ? Colors.black
-                  : wordle.doesNotExistsInTarget
-                  ? Colors.grey
-                  : Colors.white,
+          color: wordle.existsInTarget ? Colors.white70 : Colors.white,
         ),
       ),
     );
