f4d93816c4bc4e2fc7bee525d9e941b6507759ca
Toprak <topraktas19@hotmail.com>
2025-05-22 00:12:23 +0300

    isCorrectSpot added

--- lib/wordle.dart
+++ lib/wordle.dart
@@ -2,10 +2,12 @@ class Wordle {
   String letter;
   bool existsInTarget;
   bool doesNotExistsInTarget;
+  bool isCorrectSpot;
 
   Wordle({
     required this.letter,
     this.existsInTarget = false,
     this.doesNotExistsInTarget = false,
+    this.isCorrectSpot = false,
   });
 }
